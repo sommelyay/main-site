302cceb37fa72b5e3181e82707893c7663a10a8e
Gerald van den Berg <sommelyay@gmail.com>
2013-11-21 00:54:35 -0500

    adding rails files

---
+++ jgapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jgapp::Application.config.secret_key_base = '41ea74b9c7b2715125813e715af99faa8dcf628e5a030d0980e9a1b4017465248432847a39630abf892bbcf43d57c6937ee63610bb4a677abc8c50ac485589d3'
---
+++ jgapp/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jgapp::Application.config.session_store :cookie_store, key: '_jgapp_session'
